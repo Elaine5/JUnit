8d8acabb6893cbacefe3ca6cf0371514220b9218
Elaine5 <lanne9873@gmail.com>
2022-10-21 14:28:28 -0300

    Testes e excecões

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -1,8 +1,11 @@
 package br.ce.wcaquino.servicos;
  
 
+import static br.ce.wcaquino.utils.DataUtils.isMesmaData;
+import static br.ce.wcaquino.utils.DataUtils.obterDataComDiferencaDias;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
@@ -11,7 +14,10 @@ import java.util.Date;
 import org.hamcrest.CoreMatchers;
 import org.hamcrest.Matcher;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+import org.junit.rules.ExpectedException;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
@@ -20,8 +26,17 @@ import br.ce.wcaquino.servicos.LocacaoService;
 import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoServiceTest {
+	
+	
+	// ajuda a encontrar vários erros ao mesmo tempo.
+	@Rule
+	public ErrorCollector error = new ErrorCollector();
+	
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
+	
 	@Test
-	public void teste() {
+	public void testeLocacao() throws Exception {
 		//cenario
 		
 		LocacaoService service = new LocacaoService();
@@ -31,8 +46,13 @@ public class LocacaoServiceTest {
 		//acao
 		
 		Locacao locacao = service.alugarFilme(usuario, filme);
+			
+		// verifição
+			error.checkThat(locacao.getValor(), is(equalTo(5.0)));
+			error.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+			error.checkThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)), is(true));
 		
-		
+		}
 		
 		//verificao
 		
@@ -41,13 +61,73 @@ public class LocacaoServiceTest {
 		Assert.assertEquals(5.0, locacao.getValor(), 0.01);
 		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
 		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+		 * @throws Exception 
 		*/
-				
-		assertThat(locacao.getValor(), is(equalTo(5.0)));
-		assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+
+
+	@Test(expected=Exception.class)
+	public void testLocacao_filmeSemEstoque() throws Exception {
+		//cenario
 		
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme = new Filme("Filme 1", 0, 5.0);
+				
+		//acao
+				
+		service.alugarFilme(usuario, filme);
 		
 	}
+	
+	@Test
+	public void testLocacao_filmeSemEstoque_2() {
+		//cenario
+		
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme = new Filme("Filme 1", 0, 5.0);
+				
+		//acao
+				
+		try {
+			service.alugarFilme(usuario, filme);
+			Assert.fail("Deveria ter lancado uma excecao");
+		} catch (Exception e) {
+			Assert.assertThat(e.getMessage(), is("Filme sem estoque"));
+		}
+		
+	}
+	
+	@Test
+	public void testLocacao_filmeSemEstoque_3() throws Exception {
+		//cenario
+		
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme = new Filme("Filme 1", 0, 5.0);
+				
+		
+		// necessario ficar antes da acao, dentro do cenario, para que o junit veja.
+		exception.expect(Exception.class);
+		exception.expectMessage("Filme sem estoque");
+		
+		//acao
+				
+		service.alugarFilme(usuario, filme);
+		
+		exception.expect(Exception.class);
+		exception.expectMessage("Filme sem estoque");
+		
+	}	
+
+
 
 }
+
+
+
+
+
+
+
+

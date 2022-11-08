40ad0d4990c3f0928f1b53cd9c80a3271ab44d5f
Elaine5 <lanne9873@gmail.com>
2022-11-08 12:33:13 -0300

    Att

--- src/main/java/br/ce/wcaquino/servicos/Calculadora.java
+++ src/main/java/br/ce/wcaquino/servicos/Calculadora.java
@@ -5,7 +5,7 @@ import br.ce.wcaquino.exceptions.NaoPodeDividirPorZeroException;
 public class Calculadora {
 
 	public int somar(int a, int b) {
-		
+		System.out.println("Estou executando o metodo somar");
 		return a + b;
 	}
 	
@@ -23,6 +23,10 @@ public class Calculadora {
 
 	public int divide(String a, String b) {
 		return Integer.valueOf(a) / Integer.valueOf(b);
+	}
+	
+	public void imprime() {
+		System.out.println("Passei aqui");
 	}
 
 }
